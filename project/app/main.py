b888618571780728171883ef9589eac727e23469
Brent Gawryluik <brent.gawryluik@terramera.com>
2021-12-01 20:42:51 -0800

    Finished RESTful Routes

--- project/app/main.py
+++ project/app/main.py
@@ -2,7 +2,7 @@ import logging
 
 from fastapi import FastAPI
 
-from app.api import ping
+from app.api import ping, summaries  # updated
 from app.db import init_db
 
 
@@ -12,11 +12,13 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])  # new
 
     return application
 
 
 app = create_application()
+
 
 @app.on_event("startup")
 async def startup_event():
