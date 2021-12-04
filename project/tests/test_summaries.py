c9c215015d6dcecca25dabef9828c77ecc307032
Brent Gawryluik <brent.gawryluik@terramera.com>
2021-12-03 16:15:49 -0800

    Added PUT route

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -1,10 +1,11 @@
 import json
+import re
+
+from starlette import responses
 
 
 def test_create_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
 
     assert response.status_code == 201
     assert response.json()["url"] == "https://foo.bar"
@@ -25,9 +26,7 @@ def test_create_summaries_invalid_json(test_app):
 
 
 def test_read_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get(f"/summaries/{summary_id}/")
@@ -47,9 +46,7 @@ def test_read_summary_incorrect_id(test_app_with_db):
 
 
 def test_read_all_summaries(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
     summary_id = response.json()["id"]
 
     response = test_app_with_db.get("/summaries/")
@@ -60,9 +57,7 @@ def test_read_all_summaries(test_app_with_db):
 
 
 def test_remove_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
     summary_id = response.json()["id"]
 
     response = test_app_with_db.delete(f"/summaries/{summary_id}/")
@@ -74,3 +69,66 @@ def test_remove_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.delete("/summaries/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(
+        f"/summaries/{summary_id}/", data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 200
+
+    response_dict = response.json()
+    assert response_dict["id"] == summary_id
+    assert response_dict["url"] == "https://foo.bar"
+    assert response_dict["summary"] == "updated!"
+    assert response_dict["created_at"]
+
+
+def test_update_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.put(
+        "/summaries/999/", data=json.dumps({"url": "https://foo.bar", "summary": "updated!"})
+    )
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
+
+
+def test_update_summary_invalid_json(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({}))
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "url"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+            {
+                "loc": ["body", "summary"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            },
+        ]
+    }
+
+
+def test_update_summary_invalid_keys(test_app_with_db):
+    response = test_app_with_db.post("/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.put(f"/summaries/{summary_id}/", data=json.dumps({"url": "https://foo.bar"}))
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "loc": ["body", "summary"],
+                "msg": "field required",
+                "type": "value_error.missing",
+            }
+        ]
+    }
